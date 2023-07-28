db8f43fc203431beb08a1a68413502f2fed6cf9d
Shams Minal Rahman <minal3601@gmail.com>
2023-07-27 21:55:49 -0400

    initial commit for backend

---
+++ backEndCode/src/main/java/com/mcubed/estore/BackEndCodeApplication.java
@@ -0,0 +1,13 @@
+package com.mcubed.estore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndCodeApplication.class, args);
+	}
+
+}
---
+++ backEndCode/src/test/java/com/mcubed/estore/BackEndCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcubed.estore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
