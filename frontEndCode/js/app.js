c78166dc2d03fa0638ea704d624faea17c751570
Shams Minal Rahman <minal3601@gmail.com>
2023-08-06 22:10:44 -0400

    finished home catalog, no functionality yet

--- frontEndCode/js/app.js
+++ frontEndCode/js/app.js
@@ -8,3 +8,32 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     document.getElementById('app').appendChild(button);
 });
+
+let featuredSlideIndex = 1;
+const totalFeaturedSlides = document.querySelectorAll(".slideCard").length / 3;
+
+function featuredSlideShow(index) {
+    if (index > totalFeaturedSlides) {
+        featuredSlideIndex = 1;
+    }
+    if (index < 1) {
+        featuredSlideIndex = totalFeaturedSlides;
+    }
+    const slides = document.querySelectorAll(".slideCard");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = (featuredSlideIndex - 1) * 3; i < featuredSlideIndex * 3; i++) {
+        slides[i].style.display = "block";
+    }
+}
+
+function prevSlide() {
+    featuredSlideShow(featuredSlideIndex -= 1);
+}
+
+function nextSlide() {
+    featuredSlideShow(featuredSlideIndex += 1);
+}
+
+featuredSlideShow(featuredSlideIndex);
