0b9d5e07511c9594350a76ec4adb849ec43bbfda
Shams Minal Rahman <minal3601@gmail.com>
2023-08-10 21:39:17 -0400

    finished featured and catalog fetch

--- frontEndCode/js/app.js
+++ frontEndCode/js/app.js
@@ -1,88 +1,146 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-    function displayFeaturedProducts(products) {
-        const catalogContainer = document.getElementById('featured-container');
-        catalogContainer.innerHTML = '';
-        
-        products.slice(0, 6).forEach(product => {
-            const productLink = document.createElement("a");
-            productLink.className = 'slideCardLink';
-            productLink.href = "product-page.html";
-
-            const productCard = document.createElement('div');
-            productCard.className = 'slideCard';
-            
-            const productName = document.createElement("p");
-            productName.textContent = product.name;
-            
-            const productPrice = document.createElement('p');
-            productPrice.className = 'price';
-            productPrice.textContent = `$${product.price}`;
-            
-            productCard.appendChild(productName);
-            productCard.appendChild(productPrice);
-            
-            productLink.appendChild(productCard);
-            catalogContainer.appendChild(productLink);
-        });
-        
-        totalFeaturedSlides = Math.ceil(products.length / 3);
-        featuredSlideShow(featuredSlideIndex);
-    }
     
-    function fetchProducts() {
+    function fetchProducts(callback) {
         const apiUrl = 'http://localhost:8080/api/products';
         fetch(apiUrl)
             .then(response => response.json())
             .then(products => {
                 console.log('Fetched products: ', products);
-                displayFeaturedProducts(products);
+                callback(products);
             })
             .catch(error => {
                 console.error('Error fetching products:', error);
             });
     }
-    
-    fetchProducts();
-    
-    let featuredSlideIndex = 1;
-    let totalFeaturedSlides = 0;
-    
-    function featuredSlideShow(index) {
-        if (index > totalFeaturedSlides) {
-            featuredSlideIndex = 1;
+
+    let currentSlideIndex = 0;
+    const productsPerPage = 3;
+
+    function createFeaturedSlideList(products, start, end) {
+        const featuredContainer = document.getElementById('featured-container');
+        featuredContainer.innerHTML = '';
+
+        for (let i = start; i < end; i++) {
+            const productLink = document.createElement('a');
+            productLink.className = 'slideCardLink';
+            productLink.href = 'product-page.html';
+
+            const productCard = document.createElement('div');
+            productCard.className = 'slideCard';
+
+            const productName = document.createElement('p');
+            productName.textContent = products[i].name;
+
+            const productPrice = document.createElement('p');
+            productPrice.className = 'price';
+            productPrice.textContent = `$${products[i].price}`;
+
+            productCard.appendChild(productName);
+            productCard.appendChild(productPrice);
+
+            productLink.appendChild(productCard);
+            featuredContainer.appendChild(productLink);
         }
-        if (index < 1) {
-            featuredSlideIndex = totalFeaturedSlides;
-        }
-        const slides = document.querySelectorAll(".slideCard");
-        for (let i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        for (let i = (featuredSlideIndex - 1) * 3; i < featuredSlideIndex * 3; i++) {
-            if (slides[i]) {
-                slides[i].style.display = "block";
+    }
+
+    function displaySlide(products, startIndex) {
+        const endIndex = startIndex + productsPerPage;
+        createFeaturedSlideList(products, startIndex, endIndex);
+    }
+
+    function prevSlide(products) {
+        currentSlideIndex = (currentSlideIndex - productsPerPage + products.length) % products.length;
+        displaySlide(products, currentSlideIndex);
+    }
+
+    function nextSlide(products) {
+        currentSlideIndex = (currentSlideIndex + productsPerPage) % products.length;
+        displaySlide(products, currentSlideIndex);
+    }
+
+    function fetchAndInitializeProducts() {
+        fetchProducts(products => {
+            const firstSixProducts = products.slice(0, 6);
+            displaySlide(firstSixProducts, currentSlideIndex);
+
+            const prevArrow = document.querySelector('.prev-arrow');
+            const nextArrow = document.querySelector('.next-arrow');
+
+            prevArrow.addEventListener('click', () => prevSlide(firstSixProducts));
+            nextArrow.addEventListener('click', () => nextSlide(firstSixProducts));
+        });
+    }
+
+    fetchAndInitializeProducts();
+
+    let currentPage = 1;
+    const catalogProdPerPage = 12;
+    let products = [];
+    
+    function displayProducts(startIndex, endIndex) {
+        const productContainer = document.querySelector('.product-list');
+        productContainer.innerHTML = '';
+
+        for (let i = startIndex; i < endIndex; i++) {
+            const product = products[i];
+
+            if (!product) {
+                break;
             }
+
+            const productCard = document.createElement('div');
+            productCard.className = 'product-card';
+
+            const image = document.createElement('img');
+            image.src = product.imageSrc;
+            image.alt = product.name;
+
+            const title = document.createElement('h3');
+            title.textContent = product.name;
+
+            const price = document.createElement('p');
+            price.className = 'price';
+            price.textContent = `$${product.price.toFixed(2)}`;
+
+            productCard.appendChild(image);
+            productCard.appendChild(title);
+            productCard.appendChild(price);
+
+            productContainer.appendChild(productCard);
         }
     }
-    
-    function prevSlide() {
-        featuredSlideShow(featuredSlideIndex -= 1);
+
+    function updatePaginationButtons() {
+        const prevButton = document.querySelector('#prev-catalog-arrow');
+        const nextButton = document.querySelector('#next-catalog-arrow');
+
+        prevButton.disabled = currentPage === 1;
+        nextButton.disabled = products.length <= currentPage * catalogProdPerPage;
     }
-    
-    function nextSlide() {
-        featuredSlideShow(featuredSlideIndex += 1);
+
+    function loadPage(page) {
+        currentPage = page;
+        const startIndex = (page - 1) * catalogProdPerPage;
+        const endIndex = startIndex + catalogProdPerPage;
+        displayProducts(startIndex, endIndex);
+        updatePaginationButtons();
     }
-    
-    const prevArrow = document.querySelector(".prev-arrow");
-    const nextArrow = document.querySelector(".next-arrow");
-    
-    prevArrow.addEventListener("click", prevSlide);
-    nextArrow.addEventListener("click", nextSlide);
-    
-    featuredSlideShow(featuredSlideIndex);
 
-}
+    const prevButton = document.querySelector('#prev-catalog-arrow');
+    prevButton.addEventListener('click', () => {
+        if (currentPage > 1) {
+            loadPage(currentPage - 1);
+        }
+    });
 
-);
+    const nextButton = document.querySelector('#next-catalog-arrow');
+    nextButton.addEventListener('click', () => {
+        loadPage(currentPage + 1);
+    });
 
+    fetchProducts((fetchedProducts) => {
+        products = fetchedProducts;
+        loadPage(currentPage);
+    });
+
+});
